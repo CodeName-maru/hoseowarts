2a016fa82d46fefca67c783d25c13236121471b5
maru423 <jeonsy423@gmail.com>
2023-09-26 11:08:06 +0900

    master

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,4 @@
 
-let users = [
-    {
-        name: "maru",
-        gryffindor: 0,
-        slytherin: 0,
-        ravenclaw:0,
-        hufflepuff:0,
-        count:1,
-    }
-];
-
 export const home = (req, res)=> res.render("home");
 export const getLogin = (req, res)=> res.render("login");
 export const postLogin = (req, res)=> {
