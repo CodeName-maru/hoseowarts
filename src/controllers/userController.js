4c3f6f275cc675d0d3a437b07b45b6c018d89225
maru423 <jeonsy423@gmail.com>
2023-09-27 10:23:00 +0900

    master

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,15 +4,23 @@ export const home = (req, res)=> res.render("home");
 export const getLogin = (req, res)=> res.render("login");
 export const postLogin = async (req, res)=> {
     const {name} = req.body;
-    await User.create({
-        name,
-        gryffindor:0,
-        slytherin:0,
-        ravenclaw:0,
-        Hufflepuff:0,
-        chosenChild:false
-    })
-    return res.redirect("/admin")
+    try{
+        await User.create({
+            name,
+            gryffindor:0,
+            slytherin:0,
+            ravenclaw:0,
+            Hufflepuff:0,
+            chosenChild:false
+            
+        })
+        return res.redirect("/admin")
+    }
+    catch(error){
+        console.log(error);
+        return res.redirect("/admin");
+    }
+    
 };
 export const admin = async (req, res) =>{
     const users = await User.find({});
