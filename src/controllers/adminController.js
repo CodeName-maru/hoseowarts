635369c53e5151dd11ebe3c21079c10c7093d36b
maru423 <jeonsy423@gmail.com>
2023-09-29 05:11:42 +0900

    master

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -2,7 +2,7 @@ import User from "../models/User";
 import Survey from "../models/Survey";
 
 export const admin = async (req, res) =>{
-    const users = await User.find({});
+    const users = await User.find({}).sort({createdAt:"desc"});
     return res.render("admin",{users})
 }
 export const getMkSurvey = (req,res) =>{
--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+    createdAt:{type: Date, required: true, default: Date.now},
     name:String,
     answerArray:[{type:Number}],
     questNum:Number,
