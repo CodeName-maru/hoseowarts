3011869964e7bf3991be9fc1ab01eb7432bc3459
maru423 <jeonsy423@gmail.com>
2023-10-10 02:10:32 +0900

    master

--- src/init.js
+++ src/init.js
@@ -3,6 +3,7 @@ import "regenerator-runtime";
 import "./db";
 import "./models/User";
 import app from "./server";;
+import '@babel/plugin-transform-runtime'
 
 const PORT = process.env.PORT || 7000;
 const handleListening = () => console.log(`Server listening on port ${PORT}`)
