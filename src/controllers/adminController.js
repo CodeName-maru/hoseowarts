da91f54cde9358062148a81192bb4e9be0ad11f9
maru423 <jeonsy423@gmail.com>
2023-10-29 13:25:34 +0900

    master

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -4,9 +4,9 @@ import Survey from "../models/Survey";
 
 export const admin = async (req, res) =>{
     const users = await User.find({}).sort({createdAt:"desc"});
-    const griffindor = await User.countDocuments({result: 1})
-    const slyderin = await User.countDocuments({result: 2})
-    const huffulepuff= await User.countDocuments({result: 3})
-    const ravenclaw= await User.countDocuments({result: 4})
+    const griffindor = await User.countDocuments({result: "griffindor"})
+    const slyderin = await User.countDocuments({result: "slyderin"})
+    const huffulepuff= await User.countDocuments({result: "huffulepuff"})
+    const ravenclaw= await User.countDocuments({result: "ravenclaw"})
     return res.render("admin",{users,griffindor,slyderin,huffulepuff,ravenclaw})
 }
